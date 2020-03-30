d798cef799f4616c1886087ea486197b12f5482f
andywu1 <wang.hanz@husky.neu.edu>
2020-03-29 23:56:59 -0400

    lab7

---
+++ db4odemo(1)/db4odemo/src/Business/Restaurant/Restaurant.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Restaurant;
+
+import Business.UserAccount.UserAccountDirectory;
+
+/**
+ *
+ * @author harold
+ */
+public class Restaurant {
+    private UserAccountDirectory userAccountDirectory;
+
+    public UserAccountDirectory getUserAccountDirectory() {
+        return userAccountDirectory;
+    }
+
+    public void setUserAccountDirectory(UserAccountDirectory userAccountDirectory) {
+        this.userAccountDirectory = userAccountDirectory;
+    }
+    
+    
+}
